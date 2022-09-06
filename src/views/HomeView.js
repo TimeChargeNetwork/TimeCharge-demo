cacb060949104bdc952f13a5d90f59a5aeab9c9e
chrisbelloso <christianbelloso2002@gmail.com>
2022-09-06 14:38:50 -0400

    secondary container

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,7 +1,5 @@
 import './HomeView.css';
 import logo from '../img/email.LOGO.png';
-import sample1 from '../img/sampleImage.png';
-import sample5 from '../img/sampleImage5.webp';
 import sample4 from '../img/sampleImage4.png';
 import Carousel from 'react-bootstrap/Carousel';
 
@@ -17,7 +15,7 @@ const HomeView = () => {
 
         <div className='main'>
             <h2 className='mainTitle'>
-                A New Way to Charge
+                Revolutionize
             </h2>
             <p className='mainStatement'>
                 We are on a mission to revolitionize the way electric vechicles charge.
@@ -25,50 +23,54 @@ const HomeView = () => {
             </div>
 
             <div className='carouselContainer'>
-            <Carousel variant='dark'>
+                <Carousel variant='dark'>
 
-                {/* 1st Image */}
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={sample4}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <p>1st Slide</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                    {/* 1st Image */}
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={sample4}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <p>1st Slide</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-                {/* 2nd Img */}
+                    {/* 2nd Img */}
 
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={sample4}
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                        <p>2nd Slide</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={sample4}
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption>
+                            <p>2nd Slide</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-                {/* 3rd Img */}
+                    {/* 3rd Img */}
 
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={sample4}
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <p>
-                         3rd Slide
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={sample4}
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <p>
+                             3rd Slide
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </div>
-
+            <div className='secondaryContainer'>
+                <h2 className='secondaryTitle'>
+                    What EV owners feel
+                </h2>
+            </div>
         </>
     )
 }
