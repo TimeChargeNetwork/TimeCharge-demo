8775999386bce33e030bf277852d42c74f2ea123
chrisbelloso <christianbelloso2002@gmail.com>
2022-09-16 18:20:04 -0400

    sites upate

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,34 +1,12 @@
 import './AboutView.css';
-import sampleImage from '../img/sampleImage.png';
+// import sampleImage from '../img/sampleImage.png';
 
 const AboutView = () => {
     return (
         <>
-        <div className='header2'>
-            <h2 className='aboutTitle'>
-                About
-            </h2>
-        </div>
-        <div className='boxContainer'>
-            <div className='box'>
-            <p className='aboutText'>
-                This is all about the about this is just examples for us
-                to understant how all of the neccessary things will
-                follow along so that we have an understanding of how this
-                will be seen as from the consumer's perspective
-            </p>
-            <img src={sampleImage} alt="sample1" className='aboutImage'/>
-            </div>
-            <div className='box'>
-            <p className='aboutText'>
-                This is all about the about this is just examples for us
-                to understant how all of the neccessary things will
-                follow along so that we have an understanding of how this
-                will be seen as from the consumer's perspective
-            </p>
-            <img src={sampleImage} alt="sample1" className='aboutImage'/>
-            </div>
-        </div>
+        <h2>
+            This is about view
+        </h2>
         </>
     )
 }
--- src/views/SitesView.js
+++ src/views/SitesView.js
@@ -1,16 +1,20 @@
-// import MiaSkyline from '../img/Miami-Skyline.jpg';
+import MiaSkyline from '../img/Miami-Skyline.jpg';
 import './SitesView.css';
 
 const SitesView = () => {
     return(
         <>
-        <div className='main'>
-            <h2 className=''>
+        <div className='header2'>
+            <h2 className='sitesTitle'>
                 Sites
             </h2>
-        {/* <img className='miaImg' src={MiaSkyline} 
-        alt="MiaSkyline" 
-        /> */}
+        </div>
+        <div className='siteCard' style={{ backgroundImage: `url(${MiaSkyline})` }}>
+            <div className='card-content'>
+                <h2 className='card-title'>
+                    Miami
+                </h2>
+            </div>
         </div>
         </>
     )
