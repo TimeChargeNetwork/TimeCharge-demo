b98072c541c7852a67b1b7e8c9f9784eb6d0a18d
chrisbelloso <christianbelloso2002@gmail.com>
2022-09-17 19:33:10 -0400

    perfected sites

--- src/views/SitesView.js
+++ src/views/SitesView.js
@@ -1,6 +1,6 @@
 import MiaSkyline from '../img/Miami-Skyline.jpg';
-import FLL from '../img/FLL.jpg';
-import WPB from '../img/WPB.jpg';
+import FLL2 from '../img/FLL2.jpg';
+import WPB2 from '../img/WPB2.jpg';
 import './SitesView.css';
 
 const SitesView = () => {
@@ -19,14 +19,14 @@ const SitesView = () => {
                     </h2>
                 </div>
             </div>
-            <div className='siteCard' style={{ backgroundImage: `url(${FLL})` }}>
+            <div className='siteCard' style={{ backgroundImage: `url(${FLL2})` }}>
                 <div className='card-content'>
                     <h2 className='card-title'>
                         Ft. Lauderdale
                     </h2>
                 </div>
             </div>
-            <div className='siteCard' style={{ backgroundImage: `url(${WPB})` }}>
+            <div className='siteCard' style={{ backgroundImage: `url(${WPB2})` }}>
                 <div className='card-content'>
                     <h2 className='card-title'>
                         West Palm Beach
