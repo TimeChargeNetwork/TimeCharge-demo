9421cf12d096610b8abec431726c99c2620fce1a
chrisbelloso <christianbelloso2002@gmail.com>
2022-09-21 23:09:09 -0400

    Progress About

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,9 +1,15 @@
 import './AboutView.css';
+import { Link } from 'react-router-dom';
 // import sampleImage from '../img/sampleImage.png';
 
 const AboutView = () => {
     return (
         <>
+        <div className='headerAbout'>
+            <h2 className='AboutTitle'>
+                About
+            </h2>
+        </div>
         <div className='batteryContainer'>
             <div className='battery'>
                 <div className='top'></div>
@@ -11,6 +17,29 @@ const AboutView = () => {
                 <div className='charge2'></div>
                 <div className='charge3'></div>
             </div>
+        </div>
+        <div className='whatIsTC-container'>
+            <div className='whatIsTC-title'>
+                <h2 className='whatIsTC-title-text'>
+                    What Is TimeCharge?
+                </h2>
+            </div>
+            <div className='whatIsTC-P'>
+                <p>
+                    TimeCharge is solving and building the future of Electric Vehicles
+                    fast charging stations accross the U.S.
+                </p>
+                <p>
+                    South Florida will be our kickstart <Link to="/sites"> location </Link>. As our company 
+                    grows, locations will expand with it. Our MISSION is to scale accross the U.S. 
+                    Bringing better quality, reliability and services to the EV fast charging industry.
+                </p>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
         </div>
         </>
     )
